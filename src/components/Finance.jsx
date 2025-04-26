e14adc2e7233b4ad5810861ee065244936b71c0c
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-04-26 17:30:24 +0600

    designed-company-profile-section

---
+++ src/components/Finance.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+
+const Finance = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="my-8 w-11/12 mx-auto bg-white py-8 rounded-2xl">
+      <p className="text-2xl font-semibold text-center">Company Profile</p>
+
+      <div className="flex flex-col md:flex-row mt-4">
+        <div className="flex-[5] bg-gray-100 p-4 m-4 rounded-2xl">
+          <h1 className="text-center text-xl font-semibold">
+            Total Investment
+          </h1>
+          <p className="mt-4 text-2xl text-center text-green-600 font-medium p-8 bg-green-100 rounded-2xl">
+            10,000,000 BDT
+          </p>
+          <button className="mt-4 cursor-pointer bg-green-600 text-white w-full font-semibold p-2 rounded-full">
+            View Details
+          </button>
+        </div>
+        <div className="flex-[5] bg-gray-100 p-4 m-4 rounded-2xl">
+          <h1 className="text-center text-xl font-semibold">Total Expenses</h1>
+          <p className="mt-4 text-2xl text-center text-red-600 font-medium p-8 bg-red-100 rounded-2xl">
+            10,000,000 BDT
+          </p>
+          <button
+            onClick={() => navigate('/expenses')}
+            className="mt-4 cursor-pointer bg-red-600 text-white w-full font-semibold p-2 rounded-full"
+          >
+            View Details
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Finance;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import Hero from '../components/Hero';
 import Partners from '../components/Partners';
 import HouseContainer from '../components/HouseContainer';
 import { useLoaderData } from 'react-router';
+import Finance from '../components/Finance';
 
 const Home = () => {
   const houses = useLoaderData();
   return (
     <div>
       <Hero />
+      <Finance />
       <HouseContainer houses={houses} />
       <Partners />
     </div>
