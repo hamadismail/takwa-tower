01d4850672dc4f3252451ae1f7269ff23b951b80
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-04-26 13:28:16 +0600

    project-setup

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from 'react-router';
+import Root from '../layout/Root';
+import Home from '../pages/Home';
+import Error from '../pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Root,
+    errorElement: <Error />,
+    children: [{ index: true, Component: Home }],
+  },
+]);
+
+export default router;
